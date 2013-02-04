ec96d044ed1790719eec780d8c1173e54067cf21
Jim Safley <jimsafley@gmail.com>
2013-02-04 10:47:14 -0500

    i18n

--- PdfTextPlugin.php
+++ PdfTextPlugin.php
@@ -19,6 +19,7 @@ class PdfTextPlugin extends Omeka_Plugin_AbstractPlugin
     protected $_hooks = array(
         'install',
         'uninstall',
+        'initialize',
         'config_form',
         'config',
         'before_save_file',
@@ -41,11 +42,13 @@ class PdfTextPlugin extends Omeka_Plugin_AbstractPlugin
         // Don't install if the pdftotext command doesn't exist.
         // See: http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
         if ((int) shell_exec('hash pdftotext 2>&- || echo 1')) {
-            throw new Omeka_Plugin_Exception('The pdftotext command-line utility is not installed. pdftotext must be installed to install this plugin.');
+            throw new Omeka_Plugin_Exception(__('The pdftotext command-line utility ' 
+            . 'is not installed. pdftotext must be installed to install this plugin.'));
         }
         // Don't install if a PDF element set already exists.
         if ($this->_db->getTable('ElementSet')->findByName(self::ELEMENT_SET_NAME)) {
-            throw new Omeka_Plugin_Exception('An element set by the name "' . self::ELEMENT_SET_NAME . '" already exists. You must delete that element set to install this plugin.');
+            throw new Omeka_Plugin_Exception(__('An element set by the name "%s" already ' 
+            . 'exists. You must delete that element set to install this plugin.', self::ELEMENT_SET_NAME));
         }
         insert_element_set(
             array('name' => self::ELEMENT_SET_NAME, 'record_type' => 'File'),
@@ -60,6 +63,15 @@ class PdfTextPlugin extends Omeka_Plugin_AbstractPlugin
     {
         // Delete the PDF element set.
         $this->_db->getTable('ElementSet')->findByName(self::ELEMENT_SET_NAME)->delete();
+    }
+
+    /**
+     * Initialize this plugin.
+     */
+    public function hookInitialize()
+    {
+        // Add translation.
+        add_translation_source(dirname(__FILE__) . '/languages');
     }
 
     /**
